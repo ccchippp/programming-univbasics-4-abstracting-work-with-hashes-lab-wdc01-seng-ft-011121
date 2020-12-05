66bf255da8ad669520116043cd991b78b7a31943
Chip Lempke <clempke90@gmail.com>
2020-12-05 00:17:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,11 @@
 def my_hash_creator(key, value)
 {
-key: value
-}end
+key => value
+}
+end
 
 def read_from_hash(hash, key)
-  read_from_hash(hash, :key)
+  
 end
 
 def update_counting_hash(hash, key)
